6749f0d3adf617b20a7b6b90f50730b09977e7f4
Tybex.SG <tobias-riemer@gmx.de>
2021-12-27 17:56:38 +0100

    init

--- install.bat
+++ install.bat
@@ -6,7 +6,5 @@ python get-pip.py
 pip install pyautogui
 pip install pillow
 pip install opencv-python
-bitsadmin /transfer dwnlGit https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-32-bit.exe %CD%\install-git.exe
-install-git.exe
-git clone https://github.com/TobyLambda/rn-ad-bot.git app
 @pause
+call update.bat
--- update.bat
+++ update.bat
@@ -1,6 +1,3 @@
 @echo off
-del /S %CD% \app\*
 git clone https://github.com/TobyLambda/rn-ad-bot.git app
-echo "## Update complete"
-echo "## You may start the script now"
 @pause
